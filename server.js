d06a09756fff29f724108908041838d056488995
Kah919 <kahyap13@gmail.com>
2020-08-06 18:29:59 -0400

    Users can now login

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
 
-app.use(express.json());
+app.use(express.json()); // lets us read json
 
 const users = [];
 
@@ -15,6 +15,7 @@ app.get('/users', (req, res) => {
 app.post('/users', async (req, res) => {
     try {
         const salt = await bcrypt.genSalt()
+        // we can remove the salt variable and set the salt to 10 if we don't want to generate it (10 is default)
         const hashedPassword = await bcrypt.hash(req.body.password, salt)
         console.log(salt)
         console.log(hashedPassword)
@@ -28,5 +29,23 @@ app.post('/users', async (req, res) => {
 
     
 });
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name = req.body.name)
+
+    if(user === null) {
+        return res.status(400).send("Can't find user")
+    }
+
+    try {
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success')
+        } else {
+            res.send('Not Allowed')
+        }
+    } catch {
+        res.status(500).send()
+    }
+})
 
 app.listen(3000)
